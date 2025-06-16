ec5074195d110b80ed31c0cd51a99224030d5a94
prateekmishra-0 <mprateek580@gmail.com>
2025-06-16 14:01:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (89.50%) - LeetSync

---
+++ 479-largest-palindrome-product/largest-palindrome-product.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int largestPalindrome(int n) {
+        int[] x = {9,99,993,9999,99979,999999,9998017,99999999};
+        int[] y = {1,91,913,9901,99681,999001,9997647,99990001};
+        
+        return ((x[n-1] % 1337) * (y[n-1] % 1337)) % 1337;
+    }
+}
