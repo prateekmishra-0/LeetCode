8de5d1b3bbefd4ce57f90e46bb6d20739a0c46e6
prateekmishra-0 <mprateek580@gmail.com>
2025-07-22 08:59:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (27.19%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -10,15 +10,17 @@ class Solution {
         Arrays.fill(dp2, -1);
         for(int i = 0;i<n-1;i++) temp1[i] = nums[i];
         for(int i = 1;i<n;i++) temp2[i-1] = nums[i];
-        return Math.max(helper(temp1,n-2, dp),helper(temp2,n-2, dp2));
+        return Math.max(helper(temp1,n-1, dp),helper(temp2,n-1, dp2));
     }
     public int helper(int[] nums, int n, int[] dp){
-        if(n == 0) return nums[n];
-        if(n<0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int pick = nums[n] + helper(nums, n-2, dp);
-        int notPick = 0 + helper(nums, n-1, dp);
-        return dp[n] = Math.max(pick,notPick);
+        dp[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            int pick = nums[i];
+            if(i!=1) pick+=dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = Math.max(pick, notPick);
+        }
+        return dp[n-1];
 
     }
 }
