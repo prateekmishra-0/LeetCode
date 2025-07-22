692fba85ba96861d523af159759f9f0874f0dc6c
prateekmishra-0 <mprateek580@gmail.com>
2025-07-22 09:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -4,23 +4,23 @@ class Solution {
         if(n == 1) return nums[0];
         int[] temp1 = new int[n-1];
         int[] temp2 = new int[n-1];
-        int[] dp = new int[n-1];
-        Arrays.fill(dp, -1);
-        int[] dp2 = new int[n-1];
-        Arrays.fill(dp2, -1);
         for(int i = 0;i<n-1;i++) temp1[i] = nums[i];
         for(int i = 1;i<n;i++) temp2[i-1] = nums[i];
-        return Math.max(helper(temp1,n-1, dp),helper(temp2,n-1, dp2));
+        return Math.max(helper(temp1,n-1),helper(temp2,n-1));
     }
-    public int helper(int[] nums, int n, int[] dp){
-        dp[0] = nums[0];
+    public int helper(int[] nums, int n){
+        int prev = nums[0];
+        int prev2 = 0;
+        int curri = 0;
         for(int i = 1;i<n;i++){
             int pick = nums[i];
-            if(i!=1) pick+=dp[i-2];
-            int notPick = dp[i-1];
-            dp[i] = Math.max(pick, notPick);
+            if(i!=1) pick+=prev2;
+            int notPick = prev;
+            curri = Math.max(pick, notPick);
+            prev2 = prev;
+            prev = curri;
         }
-        return dp[n-1];
+        return prev;
 
     }
 }
